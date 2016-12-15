083a7ba7b66b2341ea14eb44a39cc928d8ca5e53
eremeevfd <dartfedor@mail.ru>
2016-12-15 08:29:12 +0300

    fixed manual GET-request coding using parameter params=; now using only one request to API

--- github_trending.py
+++ github_trending.py
@@ -8,18 +8,18 @@ def calculate_date_week_ago():
 
 def get_trending_repositories(top_size):
     date_week_ago = calculate_date_week_ago()
-    weekly_repos=requests.get('https://api.github.com/search/repositories?q=created:>='
-                              + str(date_week_ago)+'&sort=stars')
+    request_parametres ={
+                            'q': 'created:>='+str(date_week_ago),
+                            'sorted': 'stars'
+                        }
+    weekly_repos=requests.get('https://api.github.com/search/repositories', params=request_parametres)
     return weekly_repos.json()['items'][:top_size]
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    open_issues = requests.get('https://api.github.com/repos/'
-                               + repo_owner
-                               +'/'
-                               + repo_name
-                               + '/issues').json()
-    return len(open_issues)
+# С вашего позволения я закомментил данный метод, так как не придумал как же используя 2 метода
+# использовать только один запрос к апи гитхаба
+# def get_open_issues_amount(repo_owner, repo_name):
+#     pass
 
 
 if __name__ == '__main__':
@@ -32,4 +32,5 @@ if __name__ == '__main__':
               'Name: ' + repository['name'] + ' |',
               'URL: ' + repository['url'] + ' |',
               'stars: ' + str(repository['stargazers_count']) + ' |',
-              'Open issues: ' + str(get_open_issues_amount(repository['owner']['login'], repository['name'])))
+              'Open issues: ' + str(repository['open_issues'])
+         )
