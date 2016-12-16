a38273ba290a0c8eae2db297c80d75f090655565
eremeevfd <dartfedor@mail.ru>
2016-12-16 09:38:50 +0300

    refactored code, using format string instead of concatenation, fixed indents lines 11-14, deleted unused comments

--- github_trending.py
+++ github_trending.py
@@ -8,29 +8,21 @@ def calculate_date_week_ago():
 
 def get_trending_repositories(top_size):
     date_week_ago = calculate_date_week_ago()
-    request_parametres ={
-                            'q': 'created:>='+str(date_week_ago),
-                            'sorted': 'stars'
-                        }
-    weekly_repos=requests.get('https://api.github.com/search/repositories', params=request_parametres)
+    request_parametres = {'q': 'created:>={date_week_ago}'.format(date_week_ago=str(date_week_ago)),
+                          'sorted': 'stars'}
+    weekly_repos = requests.get('https://api.github.com/search/repositories', params=request_parametres)
     return weekly_repos.json()['items'][:top_size]
-
-
-# С вашего позволения я закомментил данный метод, так как не придумал как же используя 2 метода
-# использовать только один запрос к апи гитхаба
-# def get_open_issues_amount(repo_owner, repo_name):
-#     pass
 
 
 if __name__ == '__main__':
     top_size = 20
-    trending_repositories_created_for_a_week=get_trending_repositories(top_size)
-    print("Today is: " + str(datetime.date.today()))
-    print('Top ' + str(top_size) +' repositories created for last week: ')
+    trending_repositories_created_for_a_week = get_trending_repositories(top_size)
+    print("Today is: {today_date}".format(today_date=str(datetime.date.today())))
+    print('Top {top_size} repositories created for last week: '.format(top_size=top_size))
     for number, repository in enumerate(trending_repositories_created_for_a_week, 1):
         print(number,
-              'Name: ' + repository['name'] + ' |',
-              'URL: ' + repository['url'] + ' |',
-              'stars: ' + str(repository['stargazers_count']) + ' |',
-              'Open issues: ' + str(repository['open_issues'])
-         )
+              'Name: {name} | '.format(name=repository['name']),
+              'URL: {url} | '.format(url=repository['url']),
+              'stars: {stars} | '.format(stars=str(repository['stargazers_count'])),
+              'Open issues: {open_issues}'.format(open_issues=str(repository['open_issues']))
+              )
