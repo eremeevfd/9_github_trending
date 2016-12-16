1f52a859693767331d7d51cfabaf63d72ced5995
eremeevfd <dartfedor@mail.ru>
2016-12-16 23:15:19 +0300

    removed unnecessary str() functions

--- github_trending.py
+++ github_trending.py
@@ -8,7 +8,7 @@ def calculate_date_week_ago():
 
 def get_trending_repositories(top_size):
     date_week_ago = calculate_date_week_ago()
-    request_parametres = {'q': 'created:>={date_week_ago}'.format(date_week_ago=str(date_week_ago)),
+    request_parametres = {'q': 'created:>={date_week_ago}'.format(date_week_ago=date_week_ago),
                           'sorted': 'stars'}
     weekly_repos = requests.get('https://api.github.com/search/repositories', params=request_parametres)
     return weekly_repos.json()['items'][:top_size]
@@ -17,12 +17,12 @@ def get_trending_repositories(top_size):
 if __name__ == '__main__':
     top_size = 20
     trending_repositories_created_for_a_week = get_trending_repositories(top_size)
-    print("Today is: {today_date}".format(today_date=str(datetime.date.today())))
+    print("Today is: {today_date}".format(today_date=datetime.date.today()))
     print('Top {top_size} repositories created for last week: '.format(top_size=top_size))
     for number, repository in enumerate(trending_repositories_created_for_a_week, 1):
         print(number,
               'Name: {name} | '.format(name=repository['name']),
               'URL: {url} | '.format(url=repository['url']),
-              'stars: {stars} | '.format(stars=str(repository['stargazers_count'])),
-              'Open issues: {open_issues}'.format(open_issues=str(repository['open_issues']))
+              'stars: {stars} | '.format(stars=repository['stargazers_count']),
+              'Open issues: {open_issues}'.format(open_issues=repository['open_issues'])
               )
